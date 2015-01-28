75064442e9404c7bb443d4c7429d3bf5da2fbcf7
kradnozd <Krad@Tungs-MacBook-Air.local>
2015-01-28 21:20:25 +0700

    commit 4

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,9 +4,27 @@ BMPixel.factory('MyJsonService', function() {
   return $resource('/:data.json', {myFile: '@file'});
 });
 
-BMPixel.controller('WorkListCtrl', function($http, $scope) {
-		$http.get('data.json').success(function(data) {
-     		$scope.myWorks = data;
-     		console.log(myWorks);
-    	});
+BMPixel.controller('WorkListCtrl', function(LoadWorks, $http, $scope) {
+    LoadWorks.fetch().then(function(data) {
+	    $scope.myWorks = data;
+	});
+});
+
+BMPixel.factory('LoadWorks', function($q, $timeout, $http) {
+    var Works = {
+        fetch: function() {
+
+            var deferred = $q.defer();
+
+            $timeout(function() {
+                $http.get('data.json').success(function(data) {
+                    deferred.resolve(data);
+                });
+            }, 30);
+
+            return deferred.promise;
+        }
+    }
+
+    return Works;
 });
